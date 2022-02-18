78a784567d9100a544eccefcf56aa7ad527dc41c
Renan Pinheiro <renancamerapinheiro@gmail.com>
2022-02-17 23:05:44 -0300

    Initial Commit

---
+++ test/MariBot.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace MariBot.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/MariBot.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace MariBot.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
